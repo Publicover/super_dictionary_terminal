fcad278a98058f2a1b790780268b7e303e874a9e
Jim Publicover <jameslpublicover@gmail.com>
2018-02-08 11:04:05 -0500

    nasty formatting but urban dictionary up

--- urban_dictionary.rb
+++ urban_dictionary.rb
@@ -1,6 +1,10 @@
+require 'nokogiri'
+require 'httparty'
+require 'json'
+
 class UrbanDictionary
 
-  def urban_call(term)
+  def self.urban_call(term)
     response = HTTParty.get("https://mashape-community-urban-dictionary.p.mashape.com/define?term=#{term}",
       headers:{
         "X-MASHAPE-KEY" => ENV["MASHAPE_TEST_KEY"],
@@ -10,8 +14,25 @@ class UrbanDictionary
     if response["result_type"] == "no_results"
       "NOPE"
     else
-      response["list"][0]["definition"]
+      defs = []
+      # response["list"][0]["definition"]
+      # if response["tags"]["list"] == "definition"
+      #   defs << response["tags"]["list"]
+      # end
+      # parsed_response = JSON.parse(response.body)
+      response["list"].each do |item|
+        # if item == "definition"
+          defs << item["definition"]
+        # end
+        # puts defs
+        # puts item["definition"]
+      end
+      puts defs
     end
   end
-
 end
+
+puts 'word please'
+input = gets.chomp
+
+puts UrbanDictionary.urban_call(input)
