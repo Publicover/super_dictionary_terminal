eafced8b7656e9649e07159f83a58062686e0f21
Jim Publicover <jameslpublicover@gmail.com>
2018-02-09 10:28:28 -0500

    johnson dictionary up

--- johnson_dictionary.rb
+++ johnson_dictionary.rb
@@ -1,11 +1,18 @@
 require 'nokogiri'
 require 'httparty'
 
-# class Johnson
-  token = []
+class JohnsonDictionary
+token = ""
+# def authorized?
+#   auth = false
+# end
 
-  # def authorized?
-    # if token.empty?
+  def initialize
+    @token = String.new
+  end
+
+  def self.check_auth
+    unless @token
       response = HTTParty.post("https://samueljohnsonapi.herokuapp.com/auth/login",
         :body => {
           "name": "#{ENV['JOHNSON_API_NAME']}",
@@ -21,6 +28,24 @@ require 'httparty'
       # auth_response = JSON.parse(response.body)
     # end
     # puts token = response["auth_token"]
-    puts response
-  # end
-# end
+      @token = response['auth_token']
+    # puts token
+    end
+  end
+
+  def self.johnson_call(term)
+    check_auth
+    lookup = HTTParty.get("https://samueljohnsonapi.herokuapp.com/words/Noxious",
+      :headers => {
+        "name": "#{ENV['JOHNSON_API_NAME']}",
+        "email": "#{ENV['JOHNSON_API_EMAIL']}",
+        "password": "#{ENV['JOHNSON_API_PASS']}",
+        "Authorization": "#{@token}"
+      })
+    puts lookup['definition']
+  end
+end
+
+puts 'word please'
+input = gets.chomp
+JohnsonDictionary.johnson_call(input)
